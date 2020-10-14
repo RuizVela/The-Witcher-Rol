85ef4a69053cfa61fede81c4728315f3696b1356
Raúl <ruizvela271@gmail.com>
2020-10-14 22:58:41 +0200

    Actualizada vista de home y crear personaje.

--- views/crear.php
+++ views/crear.php
@@ -1,7 +1,7 @@
 <?php 
 require __DIR__ . "/../src/Personaje.php";
 require __DIR__ . "/../src/PersonajeController.php";
-$personaje = new Personaje($_POST['nombre'],$_POST['caracteristicas']);
+$personaje = new Personaje($_POST['nombre'],$_POST['caracteristicas'],$_POST['habilidades']);
 PersonajeController::crear($personaje);
 header("Location: home.html");
 die;
