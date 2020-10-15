7b6ab5e48b076efca1aba274531aba92480a44db
Raúl <ruizvela271@gmail.com>
2020-10-15 13:21:41 +0200

    Añadido modelo Armadura.php

---
+++ src/Armadura.php
@@ -0,0 +1,14 @@
+<?php
+
+class Armadura
+{
+    function __construct($armadura)
+    {
+        $this->cp = $armadura['cp'];
+        $this->ve = $armadura['ve'];
+        $this->localizacion = $armadura['localizacion'];
+    }
+    public $cp;
+    private $ve;
+    private $localizacion;
+}
