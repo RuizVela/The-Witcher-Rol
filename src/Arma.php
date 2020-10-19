5815d36f331c4dcef8e9c3fdabead0fde610d591
Raúl <ruizvela271@gmail.com>
2020-10-19 13:29:16 +0200

    Arma.php : Añadido atributo 'nombre'. Añadida funcion getHabilidad. Añadida funcion getCaracteristica. Añadida funcion getPrecision.

--- src/Arma.php
+++ src/Arma.php
@@ -4,12 +4,14 @@ class Arma
 {
     function __construct($arma)
     {
+        $this->nombre = $arma['nombre'];
         $this->habilidad = $arma['habilidad'];
         $this->caracteristica = $arma['caracteristica'];
         $this->precision = $arma['precision'];
         $this->daño = $arma['daño'];
         $this->fiabilidad = $arma['fiabilidad'];
     }
+    private $nombre;
     private $habilidad;
     private $caracteristica;
     private $precision;
@@ -25,5 +27,17 @@ class Arma
         }
         $daño+=$this->daño[1];
         return $daño;
+    }
+    public function getHabilidad()
+    {
+        return $this->habilidad;
+    }
+    public function getCaracteristica()
+    {
+        return $this->caracteristica;
+    }
+    public function getPrecision()
+    {
+        return $this->precision;
     }
 }
--- tests/ArmaTest.php
+++ tests/ArmaTest.php
@@ -6,7 +6,7 @@ class ArmaTest extends TestCase
 {
     public function test_dañar_devuelve_un_entero()
     {
-        $arma = array('habilidad'=>'espada','precision'=>0,'daño'=>[2,2],'fiabilidad'=>10, 'caracteristica'=>'ref');
+        $arma = array('nombre'=>'Espada de Acero','habilidad'=>'espada','precision'=>0,'daño'=>[2,2],'fiabilidad'=>10, 'caracteristica'=>'ref');
         $espada = new Arma($arma);
         $daño = $espada->dañar();
         $this->assertIsInt($daño);
